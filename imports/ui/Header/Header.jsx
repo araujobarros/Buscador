0cdc48ed888fc8ff3c19724eff7fb04b82e9de08
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-11-07 10:22:31 -0300

    Header states

--- imports/ui/Header/Header.jsx
+++ imports/ui/Header/Header.jsx
@@ -11,29 +11,45 @@ import {
 
 const Header = () => {
 
+    const [webSearch, setWebSearch] = useState('Web');
+    const [category, setCategory] = useState('Categorias');
+    const [term, setTerm] = useState('');
+
+    function handleSelect(event, setState) {
+        setState(event);
+    }
+
     return (
         <>
             <InputGroup>
                 <DropdownButton
                 variant="outline-secondary"
-                title="Web"
+                title={webSearch}
                 id="input-group-dropdown-3"
+                value={webSearch}
+                onSelect={ (event) => handleSelect(event, setWebSearch) }
                 >
-                    <Dropdown.Item href="#">Todas</Dropdown.Item>
-                    <Dropdown.Item href="#">MercadoLivre</Dropdown.Item>
-                    <Dropdown.Item href="#">Buscapé</Dropdown.Item>
+                    <Dropdown.Item eventKey="Todas">Todas</Dropdown.Item>
+                    <Dropdown.Item eventKey="MercadoLivre">MercadoLivre</Dropdown.Item>
+                    <Dropdown.Item eventKey="Buscapé">Buscapé</Dropdown.Item>
                 </DropdownButton>
                 <DropdownButton
                 variant="outline-secondary"
                 title="Categorias"
                 id="input-group-dropdown-4"
                 align="end"
+                title={category}
+                value = {category}
+                onSelect={ (event) => handleSelect(event, setCategory) }
                 >
-                    <Dropdown.Item href="#">Geladeira</Dropdown.Item>
-                    <Dropdown.Item href="#">TV</Dropdown.Item>
-                    <Dropdown.Item href="#">Celular</Dropdown.Item>
+                    <Dropdown.Item eventKey="Geladeira">Geladeira</Dropdown.Item>
+                    <Dropdown.Item eventKey="TV">TV</Dropdown.Item>
+                    <Dropdown.Item eventKey="Celular">Celular</Dropdown.Item>
                 </DropdownButton>
-                <FormControl aria-label="Text input with 2 dropdown buttons" />
+                <FormControl
+                aria-label="Text input with 2 dropdown buttons"
+                valeu={term}
+                onChange={ (event) => handleSelect(event.target.value, setTerm) }/>
                 <Button >Search</Button>
             </InputGroup>
         </>
